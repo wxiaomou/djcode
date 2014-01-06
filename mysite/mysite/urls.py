19311e0b4cc57c739b1ff7f487767b1dee9b9b38
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-05 23:05:05 -0800

    chapter 3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import index, hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
-		    url(r'^hello/$', hello),
+		url(r'^$', index),
+		url(r'^hello/$', hello),
+		url(r'^time/$', current_datetime),
+		url(r'^time/plus/(\d{1,2})$', hours_ahead),
 )
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+import datetime
+
+def index(request):
+	return HttpResponse("Index")
 
 def hello(request):
 	return HttpResponse("Hello Word!")
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body>It is now %s. </body></html>" % now
+	return HttpResponse(html)
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+#	assert False 
+	now = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html = "<html><body>It is now %s offset %s. </body></html>" % (now, offset)
+	return HttpResponse(html)
