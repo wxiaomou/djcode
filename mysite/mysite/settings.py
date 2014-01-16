e52ff236088c8b5e634c127c43720fad880fa953
Xiaomou Wang <wxm19890422@gmail.com>
2014-01-16 08:55:07 -0800

    chapter 5

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,10 +56,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'test',                      # Or path to database file if using sqlite3.
+        'USER': 'wxm',                      # Not used with sqlite3.
+        'PASSWORD': 'Bacon1234',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import index, hello, current_datetime, hours_ahead
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+		(r'^admin/', include(admin.site.urls)),
 		url(r'^$', index),
 		url(r'^hello/$', hello),
 		url(r'^time/$', current_datetime),
